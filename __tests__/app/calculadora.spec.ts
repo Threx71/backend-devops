bf7dbcd8e34a5150a18b4a6198c0752a37e28dbf
Threx71 <gonzalo4795@gmail.com>
2024-11-25 18:41:19 -0300

    lab 4 ver 2

--- __tests__/app/calculadora.spec.ts
+++ __tests__/app/calculadora.spec.ts
@@ -9,8 +9,11 @@ describe("Bateria de test de calculadora", () => {
 
     test("Sumar en la calculadora", () => {
         expect(suma(1, 2)).toBe(3);
+
         expect(suma(5, 5)).toBe(10);
+
         expect(suma(15, 15)).toBe(30);
+
         expect(suma(1, 5)).not.toBe(0);
 
         let a: any = 1;
@@ -23,8 +26,11 @@ describe("Bateria de test de calculadora", () => {
 
     test("Restar en la calculadora", () => {
         expect(restar(5, 2)).toBe(3);
+
         expect(restar(15, 5)).toBe(10);
+
         expect(restar(45, 15)).toBe(30);
+
         expect(restar(10, 5)).not.toBe(0);
 
         let a: any = 1;
@@ -75,7 +81,7 @@ describe("Bateria de test de calculadora", () => {
     test("Factorial en la calculadora", () => {
         expect(factorial(5)).toBe(120);
         expect(factorial(0)).toBe(1);
-        expect(() => factorial(-1)).toThrowError("No se puede calcular el factorial de un número negativo o no numérico");
+        expect(() => factorial(-1)).toThrowError("No se puede calcular el factorial de un número negativo");
 
         let a: any = undefined;
         expect(() => factorial(a)).toThrowError("No se puede calcular el factorial de un indefinido");
@@ -92,12 +98,19 @@ describe("Bateria de test de calculadora", () => {
         let a: any = 1;
         let b: any = "a";
         expect(operar("suma", a, b)).toBeNaN();
-        a = undefined;
-        b = 5;
-        expect(() => operar("multiplicacion", a, b)).toThrowError("No se puede multiplicar indefinidos");
+
+        a = 1;
+        b = undefined;
+        expect(() => operar("suma", a, b)).toThrowError("No se puede sumar indefinidos");
+
+        a = 1;
+        b = undefined;
+        expect(() => operar("resta", a, b)).toThrowError("No se puede restar indefinidos");
 
         expect(operar(undefined, 10, 20)).not.toBeDefined();
-        expect(operar("unknown", 10, 20)).not.toBeDefined();
+
+        expect(operar("multiplicar", 10, 20)).not.toBeDefined();
     });
+
 
 });
